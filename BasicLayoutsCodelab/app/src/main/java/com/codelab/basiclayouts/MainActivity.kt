3ecef7dab0cd72827db5fa8b1a4a96b724a91363
Cicada-3001 <cicada3319@gmail.com>
2023-09-26 02:18:55 +0300

    last-commit|

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -41,11 +42,19 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -194,7 +203,7 @@ fun FavoriteCollectionsGrid(
         contentPadding = PaddingValues(horizontal= 16.dp),
         horizontalArrangement = Arrangement.spacedBy(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = modifier
+        modifier = modifier.height(168.dp)
     ){
         items(favoriteCollectionsData){
             item -> FavoriteCollectionCard(drawable = item.drawable, text = item.text, modifier= Modifier.height(80.dp))
@@ -205,27 +214,92 @@ fun FavoriteCollectionsGrid(
 // Step: Home section - Slot APIs
 @Composable
 fun HomeSection(
-    modifier: Modifier = Modifier
+    @StringRes title:Int,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
 ) {
-    // Implement composable here
+    Column(modifier){
+        Text(
+            text= stringResource(title),
+            style = MaterialTheme.typography.titleMedium,
+            modifier= Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
 }
 
 // Step: Home screen - Scrolling
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    // Implement composable here
+    Column(
+        modifier.verticalScroll(rememberScrollState())
+    ){
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections){
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
 }
 
 // Step: Bottom navigation - Material
 @Composable
 private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
-    // Implement composable here
+   NavigationBar(
+       modifier = modifier
+   ){
+       NavigationBarItem(
+           icon = {
+               Icon(
+                   imageVector = Icons.Default.Spa,
+                   contentDescription = null
+               )
+           },
+           label = {
+               Text(
+                   text = stringResource(R.string.bottom_navigation_home)
+               )
+           },
+           selected = true,
+           onClick = {}
+       )
+       NavigationBarItem(
+           icon = {
+               Icon (
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+               )
+           },
+           label = {
+               Text(
+                   text = stringResource(R.string.bottom_navigation_profile)
+               )
+           },
+           selected = false,
+           onClick = {}
+       )
+   }
+
+
 }
 
 // Step: MySoothe App - Scaffold
 @Composable
 fun MySootheAppPortrait() {
-    // Implement composable here
+    MySootheTheme {
+        Scaffold(
+            bottomBar =  { SootheBottomNavigation()}
+        ){
+            padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
 }
 
 // Step: Bottom navigation - Material
@@ -244,8 +318,9 @@ fun MySootheAppLandscape(){
 @Composable
 fun MySootheApp() {
     MySootheTheme {
-        AlignYourBodyRow(Modifier.padding(16.dp))
+        MySootheAppPortrait()
     }
+
 }
 
 private val alignYourBodyData = listOf(
@@ -315,7 +390,11 @@ fun AlignYourBodyRowPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
 fun HomeSectionPreview() {
-    MySootheTheme { HomeSection() }
+    MySootheTheme {
+        HomeSection(R.string.align_your_body){
+            AlignYourBodyRow()
+        }
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
